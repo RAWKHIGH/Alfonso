7abfeebda565d52d1cfe5c8da6f06bd78bf85e3e
Stephen McArthur <invoiceme@hotmail.com>
2013-07-23 21:52:22 -0400

    V0.10
    Background working now and part of objects on screen

--- platform.py
+++ platform.py
@@ -42,6 +42,8 @@ manX = screen.get_width() / 2.5
 manY = floorY()
 backgroundX = 0
 backgroundY = -223
+background2X = 1028
+background2Y = -223
 groundAX = 0 
 groundAY = 577
 pipeAX = 1791
@@ -89,39 +91,43 @@ while loop:
 
 	if not jumping:
 		if keys[pygame.K_RIGHT]:
-			if not backgroundX == -5630:
+			print (backgroundX)
+			if backgroundX >= -12240:
 				backgroundX -= horzMoveAmt()
 				groundAX -= horzMoveAmt()
 				pipeAX -= horzMoveAmt()
 				pipeBX -= horzMoveAmt()
 				pipeCX -= horzMoveAmt()
 				pipeDX -= horzMoveAmt()
-				pipeEX -= horzMoveAmt()
-				pipeFX -= horzMoveAmt()
 				QboxAX -= horzMoveAmt()
 				QboxBX -= horzMoveAmt()
 				QboxCX -= horzMoveAmt()
 				QboxDX -= horzMoveAmt()
 				QboxEX -= horzMoveAmt()
 				QboxFX -= horzMoveAmt()
-				
-		
+				if backgroundX <= -5630:
+					background2X -= horzMoveAmt()
+					pipeEX -= horzMoveAmt()
+					pipeFX -= horzMoveAmt()
+						
 		if keys[pygame.K_LEFT]:
-			if not backgroundX == 0:
+			if backgroundX < 0:
 				backgroundX -= horzMoveAmt()
 				groundAX -= horzMoveAmt()
 				pipeAX -= horzMoveAmt()
 				pipeBX -= horzMoveAmt()
 				pipeCX -= horzMoveAmt()
 				pipeDX -= horzMoveAmt()
-				pipeEX -= horzMoveAmt()
-				pipeFX -= horzMoveAmt()
 				QboxAX -= horzMoveAmt()
 				QboxBX -= horzMoveAmt()
 				QboxCX -= horzMoveAmt()
 				QboxDX -= horzMoveAmt()
 				QboxEX -= horzMoveAmt()
 				QboxFX -= horzMoveAmt()
+				if backgroundX <= -5630:
+					background2X -= horzMoveAmt()
+					pipeEX -= horzMoveAmt()
+					pipeFX -= horzMoveAmt()
 				
 		
 		if keys[K_SPACE]:
@@ -139,43 +145,201 @@ while loop:
 		manY = floorY() - jumpHeight
 		
 		if keys[pygame.K_RIGHT]:
-			if not backgroundX == -5630:
+			if backgroundX >= -12240:
 				backgroundX -= horzMoveAmt()
 				groundAX -= horzMoveAmt()
 				pipeAX -= horzMoveAmt()
 				pipeBX -= horzMoveAmt()
 				pipeCX -= horzMoveAmt()
 				pipeDX -= horzMoveAmt()
-				pipeEX -= horzMoveAmt()
-				pipeFX -= horzMoveAmt()
 				QboxAX -= horzMoveAmt()
 				QboxBX -= horzMoveAmt()
 				QboxCX -= horzMoveAmt()
 				QboxDX -= horzMoveAmt()
 				QboxEX -= horzMoveAmt()
 				QboxFX -= horzMoveAmt()
+				if backgroundX <= -5630:
+					background2X -= horzMoveAmt()
+					pipeEX -= horzMoveAmt()
+					pipeFX -= horzMoveAmt()
 				
 		
 		if keys[pygame.K_LEFT]:
-			if not backgroundX == 0:
+			if backgroundX < 0:
 				backgroundX -= horzMoveAmt()
 				groundAX -= horzMoveAmt()
 				pipeAX -= horzMoveAmt()
 				pipeBX -= horzMoveAmt()
 				pipeCX -= horzMoveAmt()
 				pipeDX -= horzMoveAmt()
-				pipeEX -= horzMoveAmt()
-				pipeFX -= horzMoveAmt()
 				QboxAX -= horzMoveAmt()
 				QboxBX -= horzMoveAmt()
 				QboxCX -= horzMoveAmt()
 				QboxDX -= horzMoveAmt()
 				QboxEX -= horzMoveAmt()
 				QboxFX -= horzMoveAmt()
+				if backgroundX <= -5630:
+					background2X -= horzMoveAmt()
+					pipeEX -= horzMoveAmt()
+					pipeFX -= horzMoveAmt()
 				
+		
+		'''
+					if key[pygame.K_LEFT]:
+				player.turn = 0
+				player.image = pygame.image.load("walk L000.png")
+				if not level1A.rect.left == 0:
+					level1A.moveLeft()
+					floorw1A.moveLeft()
+					floorw1B.moveLeft()
+					floorw1C.moveLeft()
+					pipew1A.moveLeft()
+					pipew1B.moveLeft()
+					pipew1C.moveLeft()
+					pipew1D.moveLeft()
+					qboxw1A.moveLeft()
+					qboxw1B.moveLeft()
+					qboxw1C.moveLeft()
+					qboxw1D.moveLeft()
+					qboxw1E.moveLeft()
+					qboxw1F.moveLeft()
+					blockw1A.moveLeft()
+					blockw1B.moveLeft()
+					blockw1C.moveLeft()
+					blockw1D.moveLeft()
+					blockw1E.moveLeft()
+					blockw1F.moveLeft()
+					blockw1G.moveLeft()
+					blockw1H.moveLeft()
+					blockw1I.moveLeft()
+					blockw1J.moveLeft()
+					blockw1K.moveLeft()
+					blockw1L.moveLeft()
+					blockw1M.moveLeft()
+					blockw1N.moveLeft()
+					blockw1O.moveLeft()
+					blockw1P.moveLeft()
+					blockw1Q.moveLeft()
+					blockw1R.moveLeft()
+					blockw1S.moveLeft()
+					starW1A.moveLeft()
+					starW1B.moveLeft()
+					starW1C.moveLeft()
+					starW1D.moveLeft()
+					starW1E.moveLeft()
+					starW1F.moveLeft()
+					if level1A.rect.right <= 1024:
+						level1B.moveLeft()
+						floorw1D.moveLeft()
+						floorw1E.moveLeft()
+						pipew1E.moveLeft()
+						pipew1F.moveLeft()
+						qboxw1G.moveLeft()
+						qboxw1H.moveLeft()
+						qboxw1I.moveLeft()
+						qboxw1J.moveLeft()
+						qboxw1K.moveLeft()
+						qboxw1L.moveLeft()
+						qboxw1M.moveLeft()
+						blockw1T.moveLeft()
+						blockw1U.moveLeft()
+						blockw1V.moveLeft()
+						blockw1W.moveLeft()
+						blockw1X.moveLeft()
+						blockw1Y.moveLeft()
+						blockw1Z.moveLeft()
+						blockw1AA.moveLeft()
+						blockw1AB.moveLeft()
+						blockw1AC.moveLeft()
+						blockw1AD.moveLeft()
+						starW1G.moveLeft()
+						starW1H.moveLeft()
+						starW1I.moveLeft()
+						starW1J.moveLeft()
+						starW1K.moveLeft()
+						starW1L.moveLeft()
+						starW1M.moveLeft()
+
+			if key[pygame.K_RIGHT]:
+				player.turn = 1
+				player.image = pygame.image.load("walk R000.png")
+				if level1B.rect.right > 1024:
+					level1A.moveRight()
+					floorw1A.moveRight()
+					floorw1B.moveRight()
+					floorw1C.moveRight()
+					pipew1A.moveRight()
+					pipew1B.moveRight()
+					pipew1C.moveRight()
+					pipew1D.moveRight()
+					qboxw1A.moveRight()
+					qboxw1B.moveRight()
+					qboxw1C.moveRight()
+					qboxw1D.moveRight()
+					qboxw1E.moveRight()
+					qboxw1F.moveRight()
+					blockw1A.moveRight()
+					blockw1B.moveRight()
+					blockw1C.moveRight()
+					blockw1D.moveRight()
+					blockw1E.moveRight()
+					blockw1F.moveRight()
+					blockw1G.moveRight()
+					blockw1H.moveRight()
+					blockw1I.moveRight()
+					blockw1J.moveRight()
+					blockw1K.moveRight()
+					blockw1L.moveRight()
+					blockw1M.moveRight()
+					blockw1N.moveRight()
+					blockw1O.moveRight()
+					blockw1P.moveRight()
+					blockw1Q.moveRight()
+					blockw1R.moveRight()
+					blockw1S.moveRight()
+					starW1A.moveRight()
+					starW1B.moveRight()
+					starW1C.moveRight()
+					starW1D.moveRight()
+					starW1E.moveRight()
+					starW1F.moveRight()
+					if level1A.rect.right <= 1024:
+						level1B.moveRight()
+						floorw1D.moveRight()
+						floorw1E.moveRight()
+						pipew1E.moveRight()
+						pipew1F.moveRight()
+						qboxw1G.moveRight()
+						qboxw1H.moveRight()
+						qboxw1I.moveRight()
+						qboxw1J.moveRight()
+						qboxw1K.moveRight()
+						qboxw1L.moveRight()
+						qboxw1M.moveRight()
+						blockw1T.moveRight()
+						blockw1U.moveRight()
+						blockw1V.moveRight()
+						blockw1W.moveRight()
+						blockw1X.moveRight()
+						blockw1Y.moveRight()
+						blockw1Z.moveRight()
+						blockw1AA.moveRight()
+						blockw1AB.moveRight()
+						blockw1AC.moveRight()
+						blockw1AD.moveRight()
+						starW1G.moveRight()
+						starW1H.moveRight()
+						starW1I.moveRight()
+						starW1J.moveRight()
+						starW1K.moveRight()
+						starW1L.moveRight()
+						starW1M.moveRight()
+		'''
+		
 		
 		
 	screen.blit(background, (backgroundX, backgroundY))
+	screen.blit(background2, (background2X, background2Y))
 	screen.blit(pipeA, (pipeAX, pipeAY))
 	screen.blit(pipeB, (pipeBX, pipeBY))
 	screen.blit(pipeC, (pipeCX, pipeCY))
